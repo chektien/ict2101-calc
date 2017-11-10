00e70136999ed24210f4aef1ce1cebb13ace0f99
chektien <chek@gamesstudio.org>
2017-11-10 13:00:33 +0800

    adjusted formula'

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
   public int evaluate(String expression) {
     int sum = 0;
     for (String summand: expression.split("\\+"))
-      sum += Integer.valueOf(summand) + 1;
+      sum += Integer.valueOf(summand);
     return sum;
   }
 }
