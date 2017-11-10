64848331a4bf86186656c898d704d7676501494d
chektien <chek@gamesstudio.org>
2017-11-10 15:01:21 +0800

    added even more comments

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
   public int evaluate(String expression) {
     int sum = 0;
 
-    // a new comment
+    // a new comment (this is DEV2's addition)
     for (String summand: expression.split("\\+"))
       sum += Integer.valueOf(summand);
     return sum;
