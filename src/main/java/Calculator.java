5767aa445a61fffa2f4b9c020b72d818bacc864b
chektien <chek@gamesstudio.org>
2017-11-10 15:59:21 +0800

    changed back to no _1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
 
     // a new comment (I am the OWNER but I also include DEV2)
     for (String summand: expression.split("\\+"))
-      sum += Integer.valueOf(summand) + 1;
+      sum += Integer.valueOf(summand);
     return sum;
   }
 }
