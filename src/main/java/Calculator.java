245f105185df1da276867e6ce335aeb9dafbd3d3
chektien <chek@gamesstudio.org>
2017-11-10 15:01:57 +0800

    added OWNER comment

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
   public int evaluate(String expression) {
     int sum = 0;
 
-    // a new comment
+    // a new comment (I am the OWNER)
     for (String summand: expression.split("\\+"))
       sum += Integer.valueOf(summand);
     return sum;
